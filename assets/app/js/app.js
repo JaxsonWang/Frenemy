bb3d9e127446c4dd588329ee00086d3ef6aae02b
Jaxson <i@iiong.com>
2019-08-28 21:32:48 +0800

    更新资源

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -1,4 +1,9 @@
 'use strict';
+var loadFiles = {
+  js: [],
+  css: []
+};
+
 (function ($, pivot) {
   $(function () {
     //初始化顶栏透明
@@ -48,6 +53,43 @@
     touch: false
   });
 
+  //Prism高亮支持
+  loadCSS('//cdn.jsdelivr.net/npm/prismjs@1.15.0/themes/prism-tomorrow.min.css');
+  loadScript('//cdn.jsdelivr.net/npm/prismjs/components/prism-core.min.js', function () {
+    loadScript('//cdn.jsdelivr.net/npm/prismjs/plugins/autoloader/prism-autoloader.min.js', function () {
+        //将html代码块支持高亮
+        $('.post-content pre code').attr('class', function (i, clazz) {
+          if (clazz !== undefined) {
+            return clazz.replace(/language-html/g, 'language-markup');
+          }
+        });
+        //设置高亮语言样式文件地址
+        if (window.Prism !== 'undefined') {
+          Prism.plugins.autoloader.languages_path = '//cdn.jsdelivr.net/npm/prismjs/components/';
+          Prism.highlightAll();
+        }
+      }
+    );
+  });
+  //行号
+  loadCSS('//cdn.jsdelivr.net/npm/prismjs/plugins/line-numbers/prism-line-numbers.min.css');
+  loadScript('//cdn.jsdelivr.net/npm/prismjs/plugins/line-numbers/prism-line-numbers.min.js');
+  //支持行号显示
+  $('.post-content pre').addClass('line-numbers');
+  //显示语言或者粘贴
+  loadCSS('//cdn.jsdelivr.net/npm/prismjs/plugins/toolbar/prism-toolbar.min.css');
+  loadScript('//cdn.jsdelivr.net/npm/prismjs/plugins/toolbar/prism-toolbar.min.js');
+  loadScript('//cdn.jsdelivr.net/npm/prismjs/plugins/show-language/prism-show-language.min.js');
+  loadScript('//cdn.jsdelivr.net/npm/clipboard/dist/clipboard.min.js');
+  loadScript('//cdn.jsdelivr.net/npm/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js');
+
+  //图箱支持
+  loadScript('//cdn.jsdelivr.net/npm/medium-zoom/dist/medium-zoom.min.js', function () {
+    var zoom = mediumZoom(document.querySelectorAll('.post-content img'));
+    zoom.on('open', function (event) {
+    })
+  });
+
   /**
    * 添加导航样式
    */
@@ -70,3 +112,62 @@
     return $(document).scrollTop() <= 0
   }
 })(window.jQuery, window.pivot);
+
+/**
+ * 动态加载JS文件的方法
+ * Load javascript file method
+ *
+ * @param {String}   fileName              JS文件名
+ * @param {Function} [callback=function()] 加载成功后执行的回调函数
+ * @param {String}   [into='head']         嵌入页面的位置
+ */
+function loadScript(fileName, callback, into) {
+  into = into || 'body';
+  callback = callback || function () {
+  };
+  var script = null;
+  script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = fileName;
+  script.onload = function () {
+    loadFiles.js.push(fileName);
+    callback();
+  };
+  if (into === 'head') {
+    document.getElementsByTagName('head')[0].appendChild(script);
+  } else {
+    document.body.appendChild(script);
+  }
+}
+
+/**
+ * 动态加载CSS文件的方法
+ * Load css file method
+ *
+ * @param {String}   fileName              CSS文件名
+ * @param {Function} [callback=function()] 加载成功后执行的回调函数
+ * @param {String}   [into='head']         嵌入页面的位置
+ */
+function loadCSS(fileName, callback, into) {
+  into = into || 'head';
+  callback = callback || function () {
+  };
+
+  var css = document.createElement('link');
+  css.type = 'text/css';
+  css.rel = 'stylesheet';
+  css.onload = css.onreadystatechange = function () {
+    loadFiles.css.push(fileName);
+    callback();
+  };
+  css.href = fileName;
+  if (into === 'head') {
+    document.getElementsByTagName('head')[0].appendChild(css);
+  } else {
+    document.body.appendChild(css);
+  }
+}
+
+window.onload = function () {
+  console.log('已经动态加载资源：', loadFiles);
+};
